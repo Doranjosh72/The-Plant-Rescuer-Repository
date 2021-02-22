344d945921d20062ee2df2ed638adaee9c88b912
DoranJosh72 <doranjosh742@gmail.com>
2021-02-22 12:55:32 +0000

    Added test code to unity project

---
+++ ThePlantRescuerProject/Assets/Scripts/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+   //Variables that will be used in unity
+    public string StartText;
+    public string UpdateText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(StartText); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(UpdateText);
+    }
+}
